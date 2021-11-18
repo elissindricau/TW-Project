5ca8c6f9b415b68a00f00cf2a8e6582292d2a557
Silvi00 <ghemes.silvana@gmail.com>
2021-11-18 14:57:49 +0200

    Login+Register

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 export const AppRoutes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'login',
     pathMatch: 'full',
   }, {
     path: '',
@@ -14,6 +15,13 @@ export const AppRoutes: Routes = [
         {
       path: '',
       loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+  }]}, {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+        {
+      path: '',
+      loadChildren: './layouts/auth-layout/auth-layout.module#AuthLayoutModule'
   }]},
   {
     path: '**',
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'user-cmp',
+    moduleId: module.id,
+    templateUrl: 'register.component.html'
+})
+
+export class RegisterComponent implements OnInit{
+    ngOnInit(){
+    }
+}
