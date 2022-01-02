129b8f293f28cc3eb4dda03da4740d38277fbb97
Silvi00 <ghemes.silvana@gmail.com>
2022-01-02 21:05:04 +0200

    Activitati tabel

--- src/app/pages/tableProgram/program.component.ts
+++ src/app/pages/tableProgram/program.component.ts
@@ -1,4 +1,15 @@
+import { DatePipe, formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { DataState } from 'app/enum/data-state.enum';
+import { Activitate } from 'app/interface/activitate';
+import { AppState } from 'app/interface/app-state';
+import { Consultatie } from 'app/interface/consultatie';
+import { CustomResponse } from 'app/interface/custom-response';
+import { ActivitateService } from 'app/service/activitate.service';
+import { ConsultatieService } from 'app/service/consultatie.service';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
 
 declare interface TableData {
     headerRow: string[];
@@ -12,30 +23,39 @@ declare interface TableData {
 })
 
 export class TableProgramComponent implements OnInit{
-    public tableData1: TableData;
-    public tableData2: TableData;
-    ngOnInit(){
-        // this.tableData1 = {
-        //     headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
-        //     dataRows: [
-        //         ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-        //         ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-        //         ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-        //         ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-        //         ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-        //         ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
-        //     ]
-        // };
-        // this.tableData2 = {
-        //     headerRow: [ 'ID', 'Name',  'Salary', 'Country', 'City' ],
-        //     dataRows: [
-        //         ['1', 'Dakota Rice','$36,738', 'Niger', 'Oud-Turnhout' ],
-        //         ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
-        //         ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux' ],
-        //         ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park' ],
-        //         ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten', ],
-        //         ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester' ]
-        //     ]
-        // };
-    }
+    activitati: Activitate[] = [];
+  appState$: Observable<AppState<CustomResponse>>;
+  readonly DataState = DataState;
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  constructor(private activitateService: ActivitateService, private fb: FormBuilder) { 
+  }
+
+
+  ngOnInit(): void {
+    this.appState$ = this.activitateService.activitati$
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response }
+        }),
+        startWith({ dataState: DataState.LOADING_STATE }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error });
+        })
+      );
+  }
+
+  getFuture() {
+
+    var futureDate = new Date();
+   futureDate.setDate(futureDate.getDate() + 7);
+     
+      console.log(futureDate)
+    return futureDate;
+  }
+
+  getToday() {
+
+    var date = new Date();
+    return date;
+  }
 }
